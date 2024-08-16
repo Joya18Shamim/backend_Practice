14a372015c0bcb98f1573c3011ba5047a09e377a
Joya18Shamim <shamimjoya10@gmail.com>
2024-08-16 15:39:22 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended:true,limit:"10kb"}))// jo url sub hai wo le
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+
 export { app }
